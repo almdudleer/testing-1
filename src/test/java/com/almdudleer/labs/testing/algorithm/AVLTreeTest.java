7b702507b0b2921ef13d9757a89c7c1f1472c166
Alexey Serdyukov <Alexey.Serdyukov@jetbrains.com>
2020-09-21 16:50:21 +0300

    Move snapshot utils to AVLTreeTestSnapshot

--- src/test/java/com/almdudleer/labs/testing/algorithm/AVLTreeTest.java
+++ src/test/java/com/almdudleer/labs/testing/algorithm/AVLTreeTest.java
@@ -7,13 +7,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AVLTreeTest {
     public static final String TEST_RESOURCE_ROOT = "src/test/resources/algorithm";
@@ -29,52 +24,9 @@ class AVLTreeTest {
     @MethodSource("findTestFiles")
     void snapshotTest(Path snapshotsFile) throws IOException {
         AVLTree<Double> avlTree = new AVLTree<>();
-        List<AVLTreeTestSnapshot> snapshots = parseSnapshots(snapshotsFile);
+        List<AVLTreeTestSnapshot> snapshots = AVLTreeTestSnapshot.parseSnapshots(snapshotsFile);
         for (AVLTreeTestSnapshot snapshot : snapshots) {
-            applySnapshot(avlTree, snapshot);
-        }
-    }
-
-    private static List<AVLTreeTestSnapshot> parseSnapshots(Path file) throws IOException {
-        Pattern commandRegex = Pattern.compile("###( (?<command>\\w+))?( (?<argument>\\S+))?( -> (?<result>\\S+))?");
-        List<AVLTreeTestSnapshot> snapshots = new ArrayList<>();
-        StringBuilder currentSnapshotText = new StringBuilder();
-        for (String line : Files.readAllLines(file)) {
-            Matcher m = commandRegex.matcher(line);
-            if (m.find()) {
-                String action = m.group("command");
-                String argument = m.group("argument");
-                String expectedResult = m.group("result");
-                snapshots.add(new AVLTreeTestSnapshot(
-                        currentSnapshotText.toString(),
-                        action,
-                        argument,
-                        expectedResult
-                ));
-                currentSnapshotText = new StringBuilder();
-            } else {
-                currentSnapshotText.append(line);
-            }
-        }
-        return snapshots;
-    }
-
-    private static void applySnapshot(AVLTree<Double> avlTree, AVLTreeTestSnapshot snapshot) {
-        assertEquals(snapshot.json, avlTree.getJsonSnapshot());
-        if (snapshot.nextAction != null) {
-            Double value = Double.parseDouble(snapshot.nextActionArgument);
-            switch (snapshot.nextAction) {
-                case "insert":
-                    avlTree.insert(value);
-                    break;
-                case "remove":
-                    avlTree.remove(value);
-                    break;
-                case "find":
-                    Double expectedResult = Double.parseDouble(snapshot.nextActionExpectedResult);
-                    assertEquals(expectedResult, avlTree.find(value));
-                    break;
-            }
+            AVLTreeTestSnapshot.applySnapshot(avlTree, snapshot);
         }
     }
 }
--- src/test/java/com/almdudleer/labs/testing/algorithm/AVLTreeTestSnapshot.java
+++ src/test/java/com/almdudleer/labs/testing/algorithm/AVLTreeTestSnapshot.java
@@ -1,8 +1,13 @@
 package com.almdudleer.labs.testing.algorithm;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AVLTreeTestSnapshot {
     String json;
@@ -19,5 +24,48 @@ public class AVLTreeTestSnapshot {
         this.nextAction = nextAction;
         this.nextActionArgument = nextActionArgument;
         this.nextActionExpectedResult = nextActionExpectedResult;
+    }
+
+    public static List<AVLTreeTestSnapshot> parseSnapshots(Path file) throws IOException {
+        Pattern commandRegex = Pattern.compile("###( (?<command>\\w+))?( (?<argument>\\S+))?( -> (?<result>\\S+))?");
+        List<AVLTreeTestSnapshot> snapshots = new ArrayList<>();
+        StringBuilder currentSnapshotText = new StringBuilder();
+        for (String line : Files.readAllLines(file)) {
+            Matcher m = commandRegex.matcher(line);
+            if (m.find()) {
+                String action = m.group("command");
+                String argument = m.group("argument");
+                String expectedResult = m.group("result");
+                snapshots.add(new AVLTreeTestSnapshot(
+                        currentSnapshotText.toString(),
+                        action,
+                        argument,
+                        expectedResult
+                ));
+                currentSnapshotText = new StringBuilder();
+            } else {
+                currentSnapshotText.append(line);
+            }
+        }
+        return snapshots;
+    }
+
+    public static void applySnapshot(AVLTree<Double> avlTree, AVLTreeTestSnapshot snapshot) {
+        assertEquals(snapshot.json, avlTree.getJsonSnapshot());
+        if (snapshot.nextAction != null) {
+            Double value = Double.parseDouble(snapshot.nextActionArgument);
+            switch (snapshot.nextAction) {
+                case "insert":
+                    avlTree.insert(value);
+                    break;
+                case "remove":
+                    avlTree.remove(value);
+                    break;
+                case "find":
+                    Double expectedResult = Double.parseDouble(snapshot.nextActionExpectedResult);
+                    assertEquals(expectedResult, avlTree.find(value));
+                    break;
+            }
+        }
     }
 }
